b69b84f5baed8fa129320368c30f87d2b3de0ec7
gnii <chochain@yahoo.com>
2021-11-17 07:41:20 -0800

    ~/source/comparator add signed and tests

--- source/comparator.sv
+++ source/comparator.sv
@@ -1,12 +1,13 @@
 ///
-/// ForthSuper comparator (default 32-bit)
+/// ForthSuper comparator (default 8-bit)
 ///
 typedef struct packed {
     logic      eq, neq, lt, lte, gt, gte;
 } cmp_t;
     
-module comparator #(parameter N=8, parameter S=0)
-    (a, b, o);
+module comparator #(parameter N=8)
+    (s, a, b, o);
+    input  logic s;             /// 0:unsigned, 1:signed
     input  logic [N-1:0]a;      /// operand 0
     input  logic [N-1:0]b;      /// operand 1
     output       cmp_t o;       /// 6-bit output struct
@@ -14,9 +15,9 @@ module comparator #(parameter N=8, parameter S=0)
     always_comb begin
         o.eq  = (a == b);
         o.neq = (a != b);
-        o.lt  = S ? ($signed(a) <  $signed(b)) : (a <  b);
-        o.lte = S ? ($signed(a) <= $signed(b)) : (a <= b);
-        o.gt  = !o.lte;
+        o.lt  = s ? ($signed(a) <  $signed(b)) : (a <  b);
+        o.lte = s ? ($signed(a) <= $signed(b)) : (a <= b);
+        o.gt  = !o.lte;         /// Note: these inverters add one extra delay, TODO:
         o.gte = !o.lt;
     end
 endmodule // comparator
