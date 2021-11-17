6e0d4a467f2dbabf404b9304efabf0318d423479
gnii <chochain@yahoo.com>
2021-11-17 08:58:50 -0800

    ~/test/comparator use localparam

--- test/comparator_tb.sv
+++ test/comparator_tb.sv
@@ -1,21 +1,22 @@
 ///
 /// ForthSuper comparator testbench
 ///
-module comparator_tb #(
-    parameter N   = 32,            /// 32-bit comparator
-    parameter MAX = $pow(2,N) - 1, /// upper limit
-    parameter LT = 6'b011100,
-    parameter EQ = 6'b100101,
-    parameter GT = 6'b010011
-    );
+module comparator_tb;
+    // test parameters
+    localparam N   = 32;            /// 32-bit comparator
+    localparam MAX = $pow(2,N) - 1; /// upper limit
+    localparam LT = 6'b011100;
+    localparam EQ = 6'b100101;
+    localparam GT = 6'b010011;
+    // IO
     logic [N-1:0]a;
     logic [N-1:0]b;
-    logic [5:0]uo;                /// unsigned output
-    logic [5:0]so;                /// signed output
-    
+    logic [5:0]uo;                  /// unsigned output
+    logic [5:0]so;                  /// signed output
+    // DUTs
     comparator #(N) dut_u(.s(1'b0), .a, .b, .o(uo));  /// unsigned comparator  
     comparator #(N) dut_s(.s(1'b1), .a, .b, .o(so));  /// signed comparator
-
+    // debug trace
     task check(input [5:0]o, [5:0]v); begin
         $write("t=%04t, a=%h b=%h, %x[%b] => %x[%b]", $time, a, b, o, o, v, v);
         $display(" %s", o===v ? "ok" : "err");
