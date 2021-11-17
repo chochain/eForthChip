d8f619cf654dfdd5ba274a88d174748c7170f747
gnii <chochain@yahoo.com>
2021-11-16 19:09:23 -0800

    ~/source add signed compare

--- source/comparator.sv
+++ source/comparator.sv
@@ -5,18 +5,18 @@ typedef struct packed {
     logic      eq, neq, lt, lte, gt, gte;
 } cmp_t;
     
-module comparator #(parameter N=8)
+module comparator #(parameter N=8, parameter S=0)
     (a, b, o);
-    input  logic [N-1:0]a;
-    input  logic [N-1:0]b;
-    output              cmp_t o;
+    input  logic [N-1:0]a;      /// operand 0
+    input  logic [N-1:0]b;      /// operand 1
+    output       cmp_t o;       /// 6-bit output struct
 
     always_comb begin
         o.eq  = (a == b);
         o.neq = (a != b);
-        o.lt  = (a <  b);
-        o.lte = (a <= b);
-        o.gt  = (a >  b);
-        o.gte = (a >= b);
+        o.lt  = S ? ($signed(a) <  $signed(b)) : (a <  b);
+        o.lte = S ? ($signed(a) <= $signed(b)) : (a <= b);
+        o.gt  = !o.lte;
+        o.gte = !o.lt;
     end
 endmodule // comparator
--- source/comparator_tb.sv
+++ source/comparator_tb.sv
@@ -7,9 +7,10 @@ module comparator_tb #(
     );
     logic [N-1:0]a;
     logic [N-1:0]b;
-    logic [5:0]o;                 /// ModelSim does not like type from other file, TODO:
+    logic s;
+    logic [5:0]o;                 /// ModelSim does not like types from other file, TODO:
     
-    comparator #(N) cmp0(.a, .b, .o);  /// instantiate a comparator  
+    comparator #(N) dut(.a, .b, .o);  /// instantiate a comparator  
 
     task check(input [5:0]v); begin
         #1; 
@@ -19,11 +20,12 @@ module comparator_tb #(
     endtask
 
     initial begin
+        s = 0;
         //$monitor("t=%04t, a=%h b=%h, eq,neq,lt,lte,qt,qte=%b", $time, a, b, {eq,neq,lt,lte,gt,gte});
         a = 0; b = 1;          check(6'b011100);
         a = 1;                 check(6'b100101);
         b = 0;                 check(6'b010011);
-        a = MAX; b = MAX - 1;  check(6'b010011);
+        a = MAX;               check(6'b010011);
         b = MAX;               check(6'b100101);
         a = MAX - 1;           check(6'b011100);
         b = 0;                 check(6'b010011);
