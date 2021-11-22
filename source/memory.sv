128ba2150b99f9e0578e5cb8e2bfeb37ead8c9e3
gnii <chochain@yahoo.com>
2021-11-22 08:07:03 -0800

    ~/.rdf add stack3

--- source/memory.sv
+++ source/memory.sv
@@ -1,18 +1,21 @@
 ///
 /// ForthSuper memory (default byte access)
 ///
-module memory #(parameter ASZ=4, parameter DSZ=8, parameter D=$pow(2, ASZ)-1)
-    (clk, we, a, i, o);
-    input  logic clk;           /// clock
-    input  logic we;            /// write enable
-    input  logic [ASZ-1:0]a;    /// address
-    input  logic [DSZ-1:0]i;    /// input value
-    output logic [DSZ-1:0]o;    /// output
-
-    reg [DSZ-1:0] mem[D:0];     /// memory block
+module memory #(
+    parameter ASZ=4,            /// address bus size
+    parameter DSZ=8,            /// data bus size
+    parameter N=16              /// number of cells, TODO: $pow(2,ASZ) does not work
+    ) (
+    input  logic           clk, /// clock
+    input  logic           we,  /// write enable
+    input  logic [ASZ-1:0] a,   /// address
+    input  logic [DSZ-1:0] vi,  /// input value
+    output logic [DSZ-1:0] vo   /// data output
+    );
+    logic [DSZ-1:0] mem[N-1:0]; /// memory block
 
     always_ff @(posedge clk) begin
         if (we) mem[a] <= i;
-        o <= mem[a];
+        d <= mem[a];            /// iCE40 EBR is synchronous
     end
 endmodule // memory
