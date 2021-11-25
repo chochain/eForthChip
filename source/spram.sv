924631f4d9ac80626326a3d04e20e2ce9b156987
gnii <chochain@yahoo.com>
2021-11-24 19:19:52 -0800

    ~/src,test add spram8_128k

--- source/spram.sv
+++ source/spram.sv
@@ -1,18 +1,51 @@
 ///
-/// ForthSuper 32-bit Single-Port Memory (64K block)
+/// ForthSuper Single-Port Memory
+///     spram8_128k - 128K 8-bit (byte)
+///     spram32_32k - 32K 32-bit 
 ///
-module spram64k (
+`ifndef FORTHSUPER_SPRAM
+`define FORTHSUPER_SPRAM
+module spram8_128k (
     input         clk,
     input         we,   /// we:0 read, we:1 write
-    input [3:0]   bmsk, /// byte mask
+    input [16:0]  a,    /// 128K depth
+    input [7:0]   vi,   /// byte
+    output [7:0]  vo
+    );
+    logic [15:0] vo16[0:3];  /// 16-bit output
+    logic [1:0]  cs;
+
+    assign cs = a[2:1];
+    
+    genvar i;
+    for (i = 0; i < 4; i = i + 1) begin : bank
+        SP256K m (
+            .AD(a[16:3]),
+            .DI({vi, vi}),
+            .MASKWE({a[0:0], a[0:0], !a[0:0], !a[0:0]}),
+            .WE(we),
+            .CS(a[2:1]==i),
+            .CK(clk),
+            .STDBY(1'b0),
+            .SLEEP(1'b0),
+            .PWROFF_N(1'b1),
+            .DO(vo16[i])
+        );
+    end // block: bank
+        
+    assign vo = a[0:0] ? vo16[cs][15:8] : vo16[cs][7:0];
+endmodule // spram8_128k
+/*
+module spram32_32k (
+    input         clk,
+    input         we, /// we:0 read, we:1 write
+    input [3:0]   bmsk,
     input [15:0]  a, 
     input [31:0]  vi,
     output [31:0] vo
     );
-    logic [15:0] vo16 [0:1];        // 2 16-bit output
+    logic [15:0] vo16[0:2];  // 2 16-bit output
 
-    assign vo = { vo16[0], vo16[1] };
-    
     SP256K bank0 (
         .AD(a[13:0]),
         .DI(vi[31:16]),
@@ -37,5 +70,10 @@ module spram64k (
         .PWROFF_N(1'b1),
         .DO(vo16[1])
     );
-endmodule
+    
+    assign vo = { vo16[0], vo16[1] };
+endmodule // spram32_32k
+*/
+`endif // FORTHSUPER_SPRAM
+
 
---
+++ test/spram8_tb.sv
@@ -0,0 +1,41 @@
+///
+/// ForthSuper Single-Port Memory Testbench
+///
+`timescale 1ps / 1ps
+module spram8_tb;
+    localparam ASZ  = 17;   // 128K
+    localparam DSZ  = 8;    // 8-bit data
+    logic clk, we;
+    logic [ASZ-1:0] a;
+    logic [DSZ-1:0] vi, vo;
+
+    spram8_128k u1(.clk, .we, .a, .vi, .vo);
+
+    always #10 clk  = ~clk;
+
+    initial begin
+        {clk, we, a, vi} = 0;
+
+        // init clock
+        repeat(2) @(posedge clk);
+
+        // write
+        for (integer i = 0; i < ASZ; i = i + 1) begin
+            repeat(1) @(posedge clk) begin
+                a  = i;
+                we = 1;
+                vi = i;
+            end
+        end
+        // read
+        for (integer i = 0; i < ASZ + 4; i = i + 1) begin
+            repeat(1) @(posedge clk) begin
+                a  = i;
+                we = 0;
+                $display("%d[%x]: %x => %x", i, a, i, vo);
+            end
+        end
+
+        #20 $finish;
+    end       
+endmodule // spram8_tb
