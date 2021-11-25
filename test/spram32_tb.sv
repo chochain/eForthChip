3271b50c3806ad4d36c212b9a4bde9031bf84529
gnii <chochain@yahoo.com>
2021-11-24 19:20:43 -0800

    ~/test rename spram_tb to spram32_tb

--- test/spram32_tb.sv
+++ test/spram32_tb.sv
@@ -2,7 +2,7 @@
 /// ForthSuper Single-Port Memory Testbench
 ///
 `timescale 1ps / 1ps
-module spram_tb;
+module spram32_tb;
     localparam ASZ  = 16;   // 64K
     localparam DSZ  = 32;   // 32-bit data
     logic clk, we;
@@ -10,13 +10,13 @@ module spram_tb;
     logic [ASZ-1:0] a;
     logic [DSZ-1:0] vi, vo;
 
-    spram64k u1(.clk, .we, .bmsk, .a, .vi, .vo);
+    spram32_32k u1(.clk, .we, .bmsk, .a, .vi, .vo);
 
     always #10 clk  = ~clk;
 
     initial begin
-        {clk, we, a, vi, vo}  = 0;
-        bmsk = 4'b1011;
+        {clk, we, a, vi}  = 0;
+        bmsk = 4'b1111;
 
         // init clock
         repeat(2) @(posedge clk);
@@ -30,14 +30,14 @@ module spram_tb;
             end
         end
         // read
-        for (integer i = 0; i < ASZ; i = i + 1) begin
+        for (integer i = 0; i < ASZ + 4; i = i + 1) begin
             repeat(1) @(posedge clk) begin
                 a  = 1 << i;
                 we = 0;
-                $display("%d: %x => %x", i, -i << i, vo);
+                $display("%d[%x]: %x => %x", i, a, -i << i, vo);
             end
         end
 
         #20 $finish;
     end       
-endmodule // spram
+endmodule // spram32_tb
