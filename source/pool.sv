c31271af3e10118aa3314b377e1e794e3f0e6a1d
gnii <chochain@yahoo.com>
2021-11-28 08:03:16 -0800

    ~/src/pool use 4-always blocks (Cummings+Chambers)

--- source/pool.sv
+++ source/pool.sv
@@ -10,83 +10,87 @@ enum {
     FIND = 'h4, W1 = 'h5, W2 = 'h6, W4 = 'h7
 } pool_ops;
 enum {
-    MEM0 = 'h0, MEM1 = 'h1, WAIT = 'h2, CMP = 'h3 
+    MEM0 = 'h0, MEM1 = 'h1, CMP = 'h2, DONE = 'h3
 } pool_sts;
 
 module pool #(
-    parameter DSZ = 32,
-    parameter ASZ = 16
+    parameter DSZ = 8,
+    parameter ASZ = 17
     ) (
     input                  clk, /// clock
     input                  rst, /// reset
-    input [2:0]            op, /// opcode i.e. enum pool_op
-    input [ASZ-1:0]        ai, /// input address
-    input [DSZ-1:0]        vi, /// input data
-    output logic [1:0]     st, /// state
-    output logic           ok, /// 0:not found, 1:found
-    output logic [ASZ-1:0] ao, /// output address,
-    output logic [DSZ-1:0] vo  /// output data
+    input [2:0]            op,  /// opcode i.e. enum pool_op
+    input [ASZ-1:0]        ai,  /// input address
+    input [DSZ-1:0]        vi,  /// input data
+    output [DSZ-1:0]       vo,  /// output data (for memory read)
+    output logic           we,
+    output logic [1:0]     st,  /// state
+    output logic           bsy, /// 0:busy, 1:done
+    output logic [ASZ-1:0] ao,  /// output address 0:not found
+    output logic [ASZ-1:0] ao1
     );
     logic [ASZ-1:0]        here;       /// dictionary starting address
+    logic [1:0]            _st;
+    logic                  _bsy;
+    logic                  _eq;
+    logic [DSZ-1:0]        _vo;        /// src memory value
     logic [ASZ-1:0]        a, a0, a1;  /// string addresses
-    logic [DSZ-1:0]        vo0;
-    logic                  we;
     logic [3:0]            bmsk;
     cmp_t                  eq;
 
-    spram64k   mem(.clk, .we, .bmsk, .a, .vi, .vo);
-    comparator #(32) cmp(.s(1'b0), .a(vo0), .b(vo), .o(eq));
+    spram8_128k mem(.clk, .we, .a, .vi, .vo);
+    comparator #(8) cmp(.s(1'b0), .a(_vo), .b(vo), .o(eq));
     ///
-    /// find - state machine
+    /// find - 4-always state machine (Cummings & Chambers)
     ///
-    task find(); begin
-       case (st)
-       MEM0: begin
-            if (ai && a1) begin     // idle if a1 == 0
-                a  <= a0;           // setup address 0
-                a0 <= a0 + 4;
-                st <= MEM1;
-                ok <= 1'b0;
-            end
-       end
-       MEM1: begin
-            a  <= a1;               // setup address 1
-            a1 <= a1 + 4;
-            st <= WAIT;
-            ok <= 1'b0;
-       end
-       WAIT: begin
-            vo0 <= vo;              // fetch str0
-            ok  <= 1'b0;
-       end
-       CMP: begin                  
-            if (eq[0:0]) a1 <= 'h0; // idle if a1==0
-            ok <= eq[0:0];
-            st <= MEM0;
-       end
-       endcase
-    end            
-    endtask
-    
-    always_comb begin
-        case (op) 
-        NOP:    bmsk = 4'b0000;
-        R1: W1: bmsk = 4'b0001;
-        R2: W2: bmsk = 4'b0011;
-        R4: W4: bmsk = 4'b1111;
-        FIND:   bmsk = 4'b1111;
-        endcase
-        we = op > 'h4;
+    always_ff @(posedge clk, posedge rst) begin // clocked present state
+        if (rst) st <= MEM0;
+        else begin
+            st  <= _st;
+            bsy <= _bsy;
+        end
     end
     
-    always_ff @(posedge clk, negedge rst) begin
-        if (!rst) begin
+    always_comb begin   // logic for next state (state diagram)
+        case (st)
+        MEM0: _st = (op==FIND && _bsy) ? MEM1 : MEM0;
+        MEM1: _st = CMP;
+        CMP:  _st = eq[0:0] ? (_vo==0 ? DONE : MEM1) : DONE;
+        DONE: _st = MEM0;
+        endcase
+    end
+    
+    always_comb begin  // logic for next output
+        we   = op==W1;
+        _eq  = eq[0:0];
+        _bsy = 1'b1;
+        _vo  = vo;
+        case (st)
+        MEM0: begin 
+            a  = ai;        
+            a1 = ai + 1;
+            a0 = here;
+        end
+        MEM1: begin 
+            a  = a0;
+            a0 = a0 + 1;
+        end
+        CMP:  begin 
+            a  = a1;
+            a1 = a1 + 1;
+        end
+        DONE: _bsy = 1'b0;
+        endcase
+    end
+    
+    always_ff @(posedge clk, posedge rst) begin  // logic for current output
+        if (rst) begin
             here <= 0;
-            a0   <= here;
-            a1   <= ai;
-            st   <= MEM0;
-        end 
-        else if (op=='h0) find();
+        end
+        else begin
+            ao  <= a0; //(_eq && !_bsy) ? a0 : 'h0;
+            ao1 <= a1;
+        end            
     end
 endmodule // pool
 `endif // FORTHSUPER_POOL
