e5b2a8bdfbc0569b3e0ccb5e3fabf7ab1a71b651
gnii <chochain@yahoo.com>
2021-11-29 12:04:38 -0800

    ~/src,tst dev pool (CMP state mostly); rename states; drop comparator

--- source/pool.sv
+++ source/pool.sv
@@ -10,7 +10,8 @@ enum {
     FIND = 'h4, W1 = 'h5, W2 = 'h6, W4 = 'h7
 } pool_ops;
 enum {
-    MEM0 = 'h0, MEM1 = 'h1, CMP = 'h2, DONE = 'h3
+    MEM0 = 'h0, LFA0 = 'h1, LFA1 = 'h2, LEN  = 'h3, 
+    WORD = 'h4, TIB  = 'h5, CMP  = 'h6
 } pool_sts;
 
 module pool #(
@@ -24,22 +25,20 @@ module pool #(
     input [DSZ-1:0]        vi,  /// input data
     output [DSZ-1:0]       vo,  /// output data (for memory read)
     output logic           we,
-    output logic [1:0]     st,  /// state
+    output logic [2:0]     st,  /// state
     output logic           bsy, /// 0:busy, 1:done
-    output logic           eq,
-    output logic [ASZ-1:0] ao,  /// output address 0:not found
-    output logic [ASZ-1:0] ao1
+    output logic           hit, /// 1:found
+    output logic [ASZ-1:0] ao0, /// output address 0:not found, a0: for debugging now
+    output logic [ASZ-1:0] ao1  /// a1 for debugging
     );
-    logic [ASZ-1:0]        here;       /// dictionary starting address
-    logic [1:0]            _st;
-    logic                  _bsy;
+    logic [ASZ-1:0]        here, ctx;  /// dictionary starting address
+    logic [2:0]            _st;
     logic [DSZ-1:0]        _vo;        /// src memory value
     logic [ASZ-1:0]        a, a0, a1;  /// string addresses
-    logic [3:0]            bmsk;
-    cmp_t                  cmp_r;
+    logic [ASZ-1:0]        lfa, pfa;
+    cmp_t                  eq;
 
     spram8_128k mem(.clk, .we, .a, .vi, .vo);
-    comparator #(8) cmp(.s(1'b0), .a(_vo), .b(vo), .o(cmp_r));
     ///
     /// find - 4-always state machine (Cummings & Chambers)
     ///
@@ -50,42 +49,67 @@ module pool #(
     
     always_comb begin   // logic for next state (state diagram)
         case (st)
-        MEM0: _st = (op==FIND && bsy) ? MEM1 : MEM0;
-        MEM1: _st = CMP;
-        CMP:  _st = cmp_r[0:0] ? (vo==0 ? DONE : MEM1) : DONE;
-        DONE: _st = MEM0;
+        MEM0: _st = op==FIND ? LFA0 : MEM0;
+        LFA0: _st = bsy ? LFA1 : MEM0;                      // fetch low-byte of LFA
+        LFA1: _st = LEN;                                    // fetch high-byte of LFA
+        LEN:  _st = WORD;                                   // read word length
+        WORD: _st = TIB;                                    // read one byte from word 
+        TIB:  _st = CMP;                                    // read one byte from TIB
+        CMP:  _st = (_vo != vo || a0 == pfa) ? LFA0 : TIB; // compare and check word len
+        default: _st = MEM0;
         endcase
     end
     
     always_comb begin   // logic for next output
-        we   = op==W1;
-        bsy  = 1'b1;
+        we  = op==W1;
         case (st)
         MEM0: begin
-            a  = ai;        
-            a1 = ai + 1;
-            a0 = here;
+            a   = ai;
+            a0  = ctx;
+            bsy = op==FIND;
         end
-        MEM1: begin 
+        LFA0: begin
+            a   = a0;
+            a0  = a0 + 1;
+        end
+        LFA1: begin
+            a   = a0;
+            a0  = a0 + 1;
+        end
+        LEN: begin
+            lfa = {1'b0, vo, _vo};
+            a   = a0;
+            a0  = a0 + 1;
+        end
+        WORD: begin
+            pfa= a0 + vo;
             a  = a0;
-            a0 = a0 + 1;
+            a1 = ai;
         end
-        CMP:  begin 
+        TIB: begin
             a  = a1;
             a1 = a1 + 1;
+            a0 = a0 + 1;
         end
-        DONE: bsy = 1'b0;
+        CMP: begin
+            a  = a0;
+            if (_vo != vo || a0 == pfa) begin                // done with current word
+                if (_vo == vo || lfa == 'hffff) bsy = 1'b0;  // break on match or no more word
+                else a0 = lfa;                               // point to next word
+            end
+        end
+        default: a0 = ctx;          // nop
         endcase
     end
     
     always_ff @(posedge clk) begin  // logic for current output
         if (rst) begin              // synchronous reset (TODO: async)
-            here <= 0;
+            ctx <= 'h2b;
         end
-        else begin
-            eq  <= cmp_r[0:0];
+        else begin                  // output for next cycle
+            hit <= _vo == vo;
             _vo <= vo;
-            ao  <= a0;
+            ao0 <= a0;              // PFA: when !bsy && hit
             ao1 <= a1;
         end            
     end
--- test/pool_tb.sv
+++ test/pool_tb.sv
@@ -4,27 +4,28 @@
 `timescale 1ps / 1ps
 `include "../source/pool.sv"
 module pool_tb;
+    localparam TIB  = 0;
+    localparam DICT = 'h10;
     localparam ASZ  = 17;   // 64K
     localparam DSZ  = 8;    // 32-bit data
     logic           clk, rst, bsy;
     logic [2:0]     op;
-    logic [1:0]     st;
-    logic [ASZ-1:0] ai, ao, ao1;
+    logic [2:0]     st;
+    logic [ASZ-1:0] ai, ao0, ao1;
+    logic [15:0]    lfa, here;
     logic [DSZ-1:0] vi, vo;
-    logic           we, eq;
+    logic           we, hit;
     
-    string str_a  = "abcdefghijklmnop";
-    string str_b  = "abcd";
+    string tib = "abcd";
+    string word_list[4] = {
+        "abcd",
+        "efgh",
+        "ijkl",
+        "mnop"
+    };
 
-    pool dict(.clk, .rst, .op, .ai, .vi, .we, .vo, .st, .bsy, .eq, .ao, .ao1);
+    pool dict(.clk, .rst, .op, .ai, .vi, .we, .vo, .st, .bsy, .hit, .ao0, .ao1);
     
-    function integer calc_v(input integer i);
-        calc_v = str_a.getc(i);
-    /* 32-bit implementation
-        calc_v = str_a.getc(i) | str_a.getc(i+1)<<8 | str_a.getc(i+2)<<16 | str_a.getc(i+3)<<24;
-    */
-    endfunction        
-
     always #10 clk  = ~clk;
         
     task reset(); begin
@@ -41,24 +42,43 @@ module pool_tb;
         end            
     end    
     endtask
+    
+    task add_word(string w); begin
+        automatic integer n   = w.len();
+        automatic integer pfa = here + 3 + n; 
+        add_u8(here,     lfa & 'hff);
+        add_u8(here + 1, lfa >> 8);
+        add_u8(here + 2, n);
+        for (integer i = 0; i < n; i = i + 1) add_u8(here + 3 + i, w[i]);
+        add_u8(pfa,     'hbe);
+        add_u8(pfa + 1, 'hef);
+        
+        lfa  = here;
+        here = pfa + 2;
+    end
+    endtask;
         
     task setup_mem(); begin
         // write
-        for (integer i=0; i < str_a.len(); i = i + 1) add_u8(i, str_a.getc(i));
-        add_u8(str_a.len(), 0);
-        add_u8(4, 0);
-        for (integer i=0; i < str_b.len(); i = i + 1) add_u8(str_a.len()+1+i, str_b.getc(i));
-        add_u8(str_a.len() + 1 + str_b.len(), 0);
-        // verify - read back
+        for (integer i=0; i < tib.len(); i = i + 1) add_u8(TIB + i, tib.getc(i));
+        add_u8(TIB+tib.len(), 0);
+
+        lfa  = 'hffff;
+        here = DICT;
+        for (integer wi=0; wi < $size(word_list); wi++) begin
+            add_word(string'(word_list[wi]));
+        end;
+        $display("lfa=%x, here=%x", lfa, here);
 /*        
-        for (integer i=0; i < str_a.len() + 1 + str_b.len() + 3; i = i + 1) begin
+        // verify - read back
+        for (integer i=0; i < here + 4; i = i + 1) begin
             repeat(1) @(posedge clk) begin
                 op  = R1;
                 ai  = i;
-                $display("%x:%x=>%x", i, calc_v(i), vo);
+                $display("%x:%x", i, vo);
             end
         end
-*/        
+*/
     end
     endtask
         
@@ -66,11 +86,11 @@ module pool_tb;
         clk = 0;
         reset();
         setup_mem();
-        
+
         reset();
-        ai = 'h11;
+        ai = TIB;
         op = FIND;
-        repeat(40) @(posedge clk);
+        repeat(60) @(posedge clk);
         
         #20 $finish;
     end       
