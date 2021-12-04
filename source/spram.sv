56eb8fcb8ba710b3c4fa1cefb38ba4541b3df3f7
chochain <chochain@yahoo.com>
2021-12-03 20:43:30 -0800

    ~/src,tst/sparm8 add iBus8

--- source/spram.sv
+++ source/spram.sv
@@ -80,32 +80,36 @@ endmodule // spram32_32k
 ///
 /// single byte access for debugging
 ///
-/*
+interface iBus8;
+    logic we;
+    modport slave(input we);
+endinterface
+
 module spram8_128k (
+	iBus8.slave  bus,
     input        clk,
-    input        we,    /// we:0 read, we:1 write
     input [16:0] ai,     /// 128K depth
     input [7:0]  vi,    /// byte IO
     output logic [7:0] vo
     );
     logic [31:0] vi32, vo32;
     logic [1:0]  b, _b; /// byte index
-    logic [3:0]  bmsk;  /// decoder
+	iBus32       bus32();
     
-    spram32_32k m0(clk, we, bmsk, ai[16:2], vi32, vo32);
+    spram32_32k m0(.bus(bus32), .clk);
     
-    assign vi32 = {vi, vi, vi, vi};
-    assign b    = ai[1:0];
-    assign bmsk = 4'b1 << b;
-    assign vo   = _b[1:1]   /// byte mask from previous cycle
+	assign bus32.ai   = ai[16:2];
+    assign b          = ai[1:0];
+    assign bus32.bmsk = 4'b1 << b;
+    assign vi32       = {vi, vi, vi, vi};
+    assign vo = _b[1:1]   /// byte mask from previous cycle
             ? (_b[0:0] ? vo32[31:24] : vo32[23:16])
             : (_b[0:0] ? vo32[15:8]  : vo32[7:0]);
     
     always_ff @(posedge clk) begin
-        if (!we) _b <= b;   /// read needs to wait for one cycle
+        if (!bus.we) _b <= b;   /// read needs to wait for one cycle
     end
 endmodule // spram8_128k
-*/
 `endif // FORTHSUPER_SPRAM
 
 
