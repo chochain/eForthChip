14d6ffe655cebdc3eddb5f53dd60eecc07c93726
chochain <chochain@yahoo.com>
2021-12-03 20:23:45 -0800

    ~/src,tst/sparm32 add iBus32 interface

--- source/spram.sv
+++ source/spram.sv
@@ -5,22 +5,28 @@
 ///
 `ifndef FORTHSUPER_SPRAM
 `define FORTHSUPER_SPRAM
+interface iBus32;
+    logic        we;
+    logic [3:0]  bmsk;
+    logic [14:0] ai;
+    logic [31:0] vi;
+    logic [31:0] vo;
+    modport slave(input we, bmsk, ai, vi, output vo);
+endinterface
+
 module spram32_32k (
-    input         clk,
-    input         we,    /// we:0 read, we:1 write
-    input [3:0]   bmsk,
-    input [14:0]  a,     /// 32K depth
-    input [31:0]  vi,
-    output [31:0] vo
+    iBus32.slave  bus,
+    input         clk
     );
+    logic [3:0]  msk[1:0];
     logic [15:0] vo16[1:0][1:0];  // 4 16-bit output
     logic cs;           
 
     SP256K bank00 (
-        .AD(a[13:0]),
-        .DI(vi[31:16]),
-        .MASKWE({bmsk[3:3], bmsk[3:3], bmsk[2:2], bmsk[2:2]}),
-        .WE(we),
+        .AD(bus.ai[13:0]),
+        .DI(bus.vi[31:16]),
+        .MASKWE(msk[0]),
+        .WE(bus.we),
         .CS(~cs),
         .CK(clk),
         .STDBY(1'b0),
@@ -29,10 +35,10 @@ module spram32_32k (
         .DO(vo16[0][0])
     );
     SP256K bank01 (
-        .AD(a[13:0]),
-        .DI(vi[15:0]),
-        .MASKWE({bmsk[1:1], bmsk[1:1], bmsk[0:0], bmsk[0:0]}),
-        .WE(we),
+        .AD(bus.ai[13:0]),
+        .DI(bus.vi[15:0]),
+        .MASKWE(msk[1]),
+        .WE(bus.we),
         .CS(~cs),
         .CK(clk),
         .STDBY(1'b0),
@@ -41,10 +47,10 @@ module spram32_32k (
         .DO(vo16[0][1])
     );
     SP256K bank10 (
-        .AD(a[13:0]),
-        .DI(vi[31:16]),
-        .MASKWE({bmsk[3:3], bmsk[3:3], bmsk[2:2], bmsk[2:2]}),
-        .WE(we),
+        .AD(bus.ai[13:0]),
+        .DI(bus.vi[31:16]),
+        .MASKWE(msk[0]),
+        .WE(bus.we),
         .CS(cs),
         .CK(clk),
         .STDBY(1'b0),
@@ -53,10 +59,10 @@ module spram32_32k (
         .DO(vo16[1][0])
     );
     SP256K bank11 (
-        .AD(a[13:0]),
-        .DI(vi[15:0]),
-        .MASKWE({bmsk[1:1], bmsk[1:1], bmsk[0:0], bmsk[0:0]}),
-        .WE(we),
+        .AD(bus.ai[13:0]),
+        .DI(bus.vi[15:0]),
+        .MASKWE(msk[1]),
+        .WE(bus.we),
         .CS(cs),
         .CK(clk),
         .STDBY(1'b0),
@@ -64,16 +70,21 @@ module spram32_32k (
         .PWROFF_N(1'b1),
         .DO(vo16[1][1])
     );
-    assign cs = a[14:14];
-    assign vo = {vo16[cs][0], vo16[cs][1]};
+    assign msk = {
+        {bus.bmsk[3:3], bus.bmsk[3:3], bus.bmsk[2:2], bus.bmsk[2:2]},
+        {bus.bmsk[1:1], bus.bmsk[1:1], bus.bmsk[0:0], bus.bmsk[0:0]}
+    };
+    assign cs     = bus.ai[14:14];
+    assign bus.vo = {vo16[cs][0], vo16[cs][1]};
 endmodule // spram32_32k
 ///
 /// single byte access for debugging
 ///
+/*
 module spram8_128k (
     input        clk,
     input        we,    /// we:0 read, we:1 write
-    input [16:0] a,     /// 128K depth
+    input [16:0] ai,     /// 128K depth
     input [7:0]  vi,    /// byte IO
     output logic [7:0] vo
     );
@@ -81,10 +92,10 @@ module spram8_128k (
     logic [1:0]  b, _b; /// byte index
     logic [3:0]  bmsk;  /// decoder
     
-    spram32_32k m0(clk, we, bmsk, a[16:2], vi32, vo32);
+    spram32_32k m0(clk, we, bmsk, ai[16:2], vi32, vo32);
     
     assign vi32 = {vi, vi, vi, vi};
-    assign b    = a[1:0];
+    assign b    = ai[1:0];
     assign bmsk = 4'b1 << b;
     assign vo   = _b[1:1]   /// byte mask from previous cycle
             ? (_b[0:0] ? vo32[31:24] : vo32[23:16])
@@ -94,6 +105,7 @@ module spram8_128k (
         if (!we) _b <= b;   /// read needs to wait for one cycle
     end
 endmodule // spram8_128k
+*/
 `endif // FORTHSUPER_SPRAM
 
 
