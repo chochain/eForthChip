ceca7c00cb4dccf080e7042e878e684d80076c4e
gnii <chochain@yahoo.com>
2021-12-05 18:59:25 -0800

    ~/tst/dict_setup use mb8_io; refactor into _tb section for outer

--- test/dict_setup.sv
+++ test/dict_setup.sv
@@ -2,24 +2,22 @@
 /// ForthSuper Dictionary Setup Testbench
 ///
 `timescale 1ps / 1ps
-`include "../source/pool.sv"
-`include "../source/opcode.sv"
-module dict_setup;
-    localparam DICT = 'h100;
-    localparam ASZ  = 17;      // 64K
-    localparam DSZ  = 8;       // 32-bit data
-    
-    pool_ops op;               // MMU opcodes
-    pool_sts st;               // DEBUG: pool state
-    logic clk, rst;            // input signals
-    logic bsy, we, hit;        // output signals
-    logic [ASZ-1:0] ai;        // input address
-    logic [ASZ-1:0] ao0, ao1;  // DEBUG output addresses
-    logic [DSZ-1:0] vi, vo;    // I/O values
-
-    integer         ctx, here; // control variables
-
-    word word_list[$size(opcode)] = {
+`include "../source/forthsuper.vh"
+`include "../source/spram.sv"
+import FS1::*;
+module dict_setup #(
+    parameter DICT = 'h100,     /// starting address of dictionary
+    parameter DSZ  = 8,         /// 8-bit data path
+    parameter ASZ  = 17         /// 128K address space
+    ) (
+        mb8_io b8_if,           /// 8-bit memory bus master
+        input  clk,
+        input  en,
+        output logic [ASZ-1:0] ctx,
+        output logic [ASZ-1:0] here
+    );
+    opcode_e op;                /// opcode, for num()
+    word_s word_list[op.num()] = {
         '{ NOP,   "nop"  },
         '{ DUP,   "dup"  },
         '{ DROP,  "drop" },
@@ -48,66 +46,73 @@ module dict_setup;
         '{ MAX,   "max"  },
         '{ MIN,   "min"  }
     };
-    pool dict(.clk, .rst, .op, .ai, .vi, .we, .vo, .bsy, .hit, .st, .ao0, .ao1);
-
-    always #10 clk = ~clk;
     
-    task reset(); begin
-        repeat(1) @(posedge clk) rst = 1;
-        repeat(1) @(posedge clk) rst = 0;
-    end
-    endtask
-        
-    task add_u8([ASZ-1:0] ax, [7:0] vx); begin
+    task add_u8([16:0] ax, [7:0] vx);
         repeat(1) @(posedge clk) begin
-            op = W1;
-            ai = ax;
-            vi = vx;
-        end            
-    end    
-    endtask
+            b8_if.put_u8(ax, vx);
+        end
+    endtask: add_u8
     
-    task add_word(string w, logic [7:0] o); begin
+    task add_word(string w, logic [7:0] o);
         automatic integer n   = w.len();
         automatic integer pfa = here + 3 + n; 
         add_u8(here,     ctx & 'hff);
         add_u8(here + 1, ctx >> 8);
         add_u8(here + 2, n);
-        for (integer i = 0; i < n; i = i + 1) add_u8(here + 3 + i, w[i]);
-        add_u8(pfa,      o);
+        for (integer i = 0; i < n; i = i + 1) begin
+            add_u8(here + 3 + i, w[i]);
+        end
+        add_u8(pfa, o);
         
         ctx  = here;
         here = pfa + 1;
-    end
-    endtask; // add_word
+    endtask: add_word
 
-    task setup_mem(); begin
-        automatic integer n = $size(word_list);
+    task setup_mem;
         ctx  = 'hffff;
         here = DICT;
         // write
-        for (integer wi=0; wi < n; wi++) begin
-            add_word(string'(word_list[wi].name), word_list[wi].op);
+        foreach(word_list[i]) begin
+            add_word(string'(word_list[i].name), word_list[i].op);
         end;
+    endtask: setup_mem
+endmodule: dict_setup
+
+module dict_setup_tb;
+    localparam DICT = 'h100;      /// starting address of dictionary
+    logic clk, rst, en;
+    logic [16:0] ctx, here;
+    
+    mb8_io      b8_if();
+    spram8_128k m0(b8_if.slave, clk);
+
+    dict_setup #(DICT) dict(.*, .b8_if(b8_if.master));
+    
+    task reset;
+        repeat(1) @(posedge clk) rst = 1;
+        repeat(1) @(posedge clk) rst = 0;
+    endtask: reset
+    
+    task verify; 
         $display("lfa=%x, here=%x", ctx, here);
-/*        
         // verify - read back
         for (integer i=DICT; i < here + 4; i = i + 1) begin
             repeat(1) @(posedge clk) begin
-                op  = R1;
-                ai  = i;
-                $display("%x:%x", i, vo);
+                b8_if.get_u8(i);
+                $display("%x:%x", i, b8_if.vo);
             end
         end
-*/        
-    end
-    endtask
+    endtask: verify
+    
+    always #10 clk = ~clk;
         
     initial begin
-        clk   = 0;
+        clk = 0;
         reset();
-        setup_mem();
+        dict.setup_mem();
+        
+        verify();
         
         #20 $finish;
     end
-endmodule // dict_setup
+endmodule: dict_setup_tb
