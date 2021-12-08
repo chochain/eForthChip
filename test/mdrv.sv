05a28c0152f6d9383e326819a3ee348a527988d8
gnii <chochain@yahoo.com>
2021-12-08 09:56:36 -0800

    ~/test/mdrv add bus mux

--- test/mdrv.sv
+++ test/mdrv.sv
@@ -8,14 +8,13 @@ interface md_io;
 endinterface: md_io
 
 module drv #(N=0) (md_io.master md_if, input clk, en, output logic bsy);
-    logic [7:0] idx;
+    logic [7:0] idx = 0;
     always_ff @(posedge clk) begin
-        if (!en) idx <= 0;
-        else begin
+        if (en) begin
             md_if.we <= 1'b1;
             md_if.ai <= idx + N;
             bsy      <= (idx < 'h4);
-            idx      <= idx + 1'b0;
+            idx      <= idx + 1'b1;
         end
     end // always_ff
 endmodule: drv
@@ -23,15 +22,24 @@ endmodule: drv
 module mdrv(md_io.master md_if, input clk, ok);
     logic i, en[2], bsy[2];
     
-    drv #(0) d0(.md_if, .clk, .en(en[0]), .bsy(bsy[0]));
-    drv #(8) d1(.md_if, .clk, .en(en[1]), .bsy(bsy[1]));
+    md_io b0_if();
+    md_io b1_if();
+    drv #(0) d0(.md_if(b0_if.master), .clk, .en(en[0]), .bsy(bsy[0]));
+    drv #(8) d1(.md_if(b1_if.master), .clk, .en(en[1]), .bsy(bsy[1]));
+    
+    always_comb begin
+        case (en[0])
+        1'b0: {md_if.we, md_if.ai} = {b1_if.we, b1_if.ai};
+        1'b1: {md_if.we, md_if.ai} = {b0_if.we, b0_if.ai};
+        endcase
+    end
 
     always_ff @(posedge clk) begin
-        if (!ok) i <= 0;
-        else begin
+        if (ok) begin
             en[0] <= i;
             en[1] <= ~i;
             i     <= ~i;
         end
+        else i <= 1'b0;
     end // always_ff
 endmodule: mdrv
--- test/mdrv_tb.sv
+++ test/mdrv_tb.sv
@@ -6,14 +6,15 @@ module mdrv_tb;
     logic clk, ok;
 
     md_io md_if();                      // bus instance
-    mdrv  dr(md_if.master, clk, en);
+    mdrv  dr(.md_if(md_if.master), .clk, .ok);
     
     always #10 clk  = ~clk;
 
     initial begin
-        clk = 0;
-        ok  = 1;
-        // init clock
+        clk = 1'b0;
+        ok  = 1'b0;
+        repeat(2) @(posedge clk);
+        ok  = 1'b1;
         repeat(30) @(posedge clk);
 
         #20 $finish;
