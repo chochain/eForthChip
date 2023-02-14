c226499cc7c27eead83d27ff9b514d430ad4c717
gnii <chochain@yahoo.com>
2023-02-14 13:52:16 +0800

    ~/src/spram.sv spram32_32k fix vo hi-lo byte order

--- source/spram.sv
+++ source/spram.sv
@@ -85,7 +85,7 @@ module spram32_32k (             /// width cascade
         {b32_if.bmsk[1:1], b32_if.bmsk[1:1], b32_if.bmsk[0:0], b32_if.bmsk[0:0]}
     };
     assign cs        = b32_if.ai[14:14];             // select high range
-    assign b32_if.vo = {vo16[cs][1], vo16[cs][0]};   // slave response
+    assign b32_if.vo = {vo16[cs][0], vo16[cs][1]};   // slave response
 
     SP256K bank00 (
         .AD(b32_if.ai[13:0]),
@@ -94,8 +94,8 @@ module spram32_32k (             /// width cascade
         .WE(b32_if.we),
         .CS(~cs),
         .CK(b32_if.clk),
-        .STDBY(1'b0),
-        .SLEEP(1'b0),
+        .STDBY(1'b0),            // STDBY(cs) to save power
+        .SLEEP(1'b0),            // SLEEP(cs) to save power
         .PWROFF_N(1'b1),
         .DO(vo16[0][0])
     );
