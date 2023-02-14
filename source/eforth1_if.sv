cccbfd22db7d189e36434aaea233e7c61c3c5faf
gnii <chochain@yahoo.com>
2023-02-14 11:22:11 +0800

    ~/src/eforth1_if.sv replace 32-bit with generic memoby interface

--- source/eforth1_if.sv
+++ source/eforth1_if.sv
@@ -1,5 +1,6 @@
 ///
-/// eForth1 Bus interfaces
+/// @file
+/// @brief eForth1 -  Bus interfaces
 ///
 `ifndef EFORTH1_EFORTH1_IF
 `define EFORTH1_EFORTH1_IF
@@ -8,68 +9,78 @@ typedef enum logic [1:0] {
     SS_LOAD = 2'b0, SS_PUSH = 2'b01, SS_POP = 2'b10, SS_PICK = 2'b11 
 } sop_e;
 
-interface mb32_io #(
-    parameter ASZ=15,
-    parameter DSZ=32
+interface mb_io #(
+    parameter DSZ = 16,               // 8, 16, 24, 32
+    parameter ASZ = 17                // total 128K bytes
     )(input logic clk);
-    logic        we;
-    logic [3:0]  bmsk;
+    logic           we;
+    logic [3:0]     bmsk;
     logic [ASZ-1:0] ai;
     logic [DSZ-1:0] vi;
     logic [DSZ-1:0] vo;
-
+    
     clocking ioDrv @(posedge clk);
         default input #1 output #1;
-    endclocking // ioMaster
+    endclocking                       // ioMaster
 
-    modport master(clocking ioDrv, output we, bmsk, ai, vi);
-    modport slave(clocking ioDrv, input we, bmsk, ai, vi, output vo);
-endinterface: mb32_io
+    modport master(clocking ioDrv, output we, bmsk, ai, vi, import put, get);
+    modport slave(clocking ioDrv, input clk, we, bmsk, ai, vi, output vo);
+    
+    function void put([ASZ-1:0] ax, [DSZ-1:0] vx);
+        we = 1'b1;
+        ai = ax;
+        vi = vx;
+    endfunction: put
 
+    function logic[DSZ-1:0] get([ASZ-1:0] ax);
+        we  = 1'b0;
+        ai  = ax;
+        get = vo;
+    endfunction: get
+endinterface: mb_io
+
+///
+/// 8-bit memory block interface (for debugging, mostly)
+///
 interface mb8_io #(
     parameter ASZ=17,
-    parameter DSZ=8);
+    parameter DSZ=8
+    )(input logic clk);
     logic we;
     logic [ASZ-1:0] ai;
     logic [DSZ-1:0] vi;
     logic [DSZ-1:0] vo;
+ 
+    modport master(output we, ai, vi, import put, get);
+    modport slave(input clk, we, ai, vi, output vo);
 
-    modport master(output we, ai, vi, import put_u8, get_u8);
-    modport slave(input we, ai, vi, output vo);
+    function void put([ASZ-1:0] ax, [DSZ-1:0] vx);
+         we = 1'b1;
+         ai = ax;
+         vi = vx;
+    endfunction: put
 
-    function void put_u8([ASZ-1:0] ax, [DSZ-1:0] vx);
-        we = 1'b1;
-        ai = ax;
-        vi = vx;
-    endfunction: put_u8
+    function logic [DSZ-1:0] get([ASZ-1:0] ax);
+        we  = 1'b0;
+        ai  = ax;
+        get = vo;
+    endfunction: get
+endinterface: mb8_io
 
-    function void get_u8([ASZ-1:0] ax);
-        we = 1'b0;
-        ai = ax;
-        // return vo
-    endfunction: get_u8
-endinterface : mb8_io
-
+/*
 interface ss_io #(
     parameter DEPTH = 64,
     parameter DSZ   = 32);
-    localparam SSZ  = $clog2(DEPTH);
-    localparam NEG1 = DEPTH - 1;
     sop_e           op;
     logic [DSZ-1:0] vi;
-    logic [DSZ-1:0] s0, _tos, tos = -1;
-    logic [SSZ-1:0] sp_1, sp = 0;
+    logic [DSZ-1:0] s0, _tos, tos;
+    logic [SSZ-1:0] sp_1, sp;
     logic xt;
 
     modport master(output op, vi, import load, push, pop);
-    modport slave(input op, vi, output sp, sp_1, s0, tos, import update_tos);
+    modport slave(input op, vi, output sp, sp_1, s0, tos);
     
-    always_comb begin
-        sp_1 = sp + NEG1;
-        xt   = 1'b0;
-    end
-
-    function void load(input [DSZ-1:0] v);
+    function logic [DS-1:0] load(input [DSZ-1:0] v);
         _tos = v;
         xt   = 1'b1;
     endfunction: load
@@ -84,9 +95,6 @@ interface ss_io #(
         pop = s0;         // return from cached s0
     endfunction: pop
     
-    task update_tos;
-        if (xt) tos <= _tos;
-    endtask: update_tos;
-
 endinterface: ss_io
+*/
 `endif // EFORTH1_EFORTH1_IF
