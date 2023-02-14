5aeba26a4256432a2641739d64a452550a5f4c2d
gnii <chochain@yahoo.com>
2023-02-14 11:23:36 +0800

    ~/test/spram_tb change pass clk to b8_if; add vo calling interface.functions

--- test/spram8_tb.sv
+++ test/spram8_tb.sv
@@ -6,10 +6,11 @@
 module spram8_tb;
     localparam ASZ  = 17;   // 128K
     localparam DSZ  = 8;    // 8-bit data
-    logic clk;
+    logic       clk;
+    logic [7:0] vo;
    
-    mb8_io      b8_if();
-    spram8_128k u1(b8_if.slave, clk);
+    mb8_io      b8_if(clk);
+    spram8_128k u1(b8_if.slave);
 
     always #10 clk = ~clk;
 
@@ -19,27 +20,27 @@ module spram8_tb;
         // init clock
         repeat(2) @(negedge clk);
 
-        // byte order check
-        for (integer i = 0; i < ASZ; i = i + 1) begin
+        // byte order check (direct access interface ports)
+        for (int i = 0; i < ASZ; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.we = 1'b1;
                 b8_if.ai = i;
                 b8_if.vi = i;
+                b8_if.we = 1'b1;
             end
         end
         repeat(2) @(negedge clk);
-        for (integer i = 0; i < ASZ + 4; i = i + 1) begin
+        for (int i = 0; i < ASZ + 4; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.we = 1'b0;
                 b8_if.ai = i;
+                b8_if.we = 1'b0;
                 $display("%d[%x]: %x => %x", i, b8_if.ai, i, b8_if.vo);
             end
         end
         
         // range check
-        for (integer i = 0; i < ASZ; i = i + 1) begin
+        for (int i = 0; i < ASZ; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.put_u8(
+                b8_if.put(
                     ('h1 << i) | (i & 3),
                     (i < 8) ? ('h1 << i) : ('hff >> (i-8))
                 );
@@ -48,21 +49,21 @@ module spram8_tb;
         repeat(2) @(negedge clk);
         for (integer i = 0; i < ASZ + 4; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.get_u8(('h1 << i) | (i & 3));
-                $display("%d[%x]: %x => %x", i, b8_if.ai, i<8 ? ('h1 << i) : ('hff >> (i-8)), b8_if.vo);
+                vo = b8_if.get(('h1 << i) | (i & 3));
+                $display("%d[%x]: %x => %x", i, b8_if.ai, i<8 ? ('h1 << i) : ('hff >> (i-8)), vo);
             end
         end
         // high address check
         for (integer i = 0; i < ASZ; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.put_u8('h1ffff - i, i);
+                b8_if.put('h1ffff - i, i);
             end
         end
         repeat(2) @(posedge clk);
         for (integer i = 0; i < ASZ + 4; i = i + 1) begin
             repeat(1) @(negedge clk) begin
-                b8_if.get_u8('h1ffff - i);
-                $display("%d[%x]: %x => %x", i, b8_if.ai, i, b8_if.vo);
+                vo = b8_if.get('h1ffff - i);
+                $display("%d[%x]: %x => %x", i, b8_if.ai, i, vo);
             end
         end
         
