cc5cf7960d5a8f8ae86d68e9f216600f1d557525
Chochain Lee <chochain@yahoo.com>
2025-07-22 17:25:21 +0800

    ~/verilator/spram8_128k.v mb_io=>mb32_io (fixed size)

--- verilator/spram8_128k.v
+++ verilator/spram8_128k.v
@@ -7,7 +7,7 @@ module spram8_128k (   /// depth cascade
     );
     logic [1:0] m, _m; /// byte index of (current and previous cycle)
 
-    mb_io #(32) b32_if(b8_if.clk);
+    mb32_io     b32_if(b8_if.clk);
     spram32_32k m0(b32_if.slave);
 
     assign m           = b8_if.ai[1:0];
