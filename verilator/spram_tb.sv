bf56112ff3af805e1591c13e169a261e970d8a0b
Chochain Lee <chochain@yahoo.com>
2025-07-25 11:07:34 +0800

    ~/verilator/spram_tb.sv shared int i (reduce scopesDump)

--- verilator/spram_tb.sv
+++ verilator/spram_tb.sv
@@ -17,13 +17,14 @@ module top (
     assign vi = b8_if.vi;
 
     initial begin
+        int i;
         vo = 0;
 
         // init clock
         repeat(2) @(negedge clk);
 
         $display("byte order check (port direct write)");
-        for (int i = 0; i < ASZ; i++) begin
+        for (i = 0; i < ASZ; i++) begin
             repeat(1) @(negedge clk) begin
                 b8_if.ai = i[ASZ-1:0];
                 b8_if.vi = i[7:0];
@@ -33,7 +34,7 @@ module top (
         end
         $display("byte order check (port direct read)");
         repeat(2) @(negedge clk);
-        for (int i = 0; i < ASZ + 4; i++) begin
+        for (i = 0; i < ASZ + 4; i++) begin
             repeat(1) @(negedge clk) begin
                 b8_if.ai = i[ASZ-1:0];
                 b8_if.we = 1'b0;
@@ -42,7 +43,7 @@ module top (
         end
 
         $display("range check (write via interface)");
-        for (int i = 0; i < ASZ; i++) begin
+        for (i = 0; i < ASZ; i++) begin
             repeat(1) @(negedge clk) begin
                 b8_if.put(
                     (('h1 << i[ASZ-1:0]) | (i[ASZ-1:0] & 3)),
@@ -53,14 +54,14 @@ module top (
         end
         $display("range check (read via interface)");
         repeat(2) @(negedge clk);
-        for (integer i = 0; i < ASZ + 4; i++) begin
+        for (i = 0; i < ASZ + 4; i++) begin
             repeat(1) @(negedge clk) begin
                 vo = b8_if.get(('h1 << i[ASZ-1:0]) | (i[ASZ-1:0] & 3));
             end
         end
 
         $display("high address write");
-        for (integer i = 0; i < ASZ; i++) begin
+        for (i = 0; i < ASZ; i++) begin
             repeat(1) @(negedge clk) begin
                 b8_if.put('h1ffff - i[ASZ-1:0], i[7:0]);
                 vo = b8_if.vo;
@@ -69,7 +70,7 @@ module top (
        
         $display("high address read");
         repeat(2) @(posedge clk);
-        for (integer i = 0; i < ASZ + 4; i++) begin
+        for (i = 0; i < ASZ + 4; i++) begin
             repeat(1) @(negedge clk) begin
                 vo = b8_if.get('h1ffff - i[ASZ-1:0]);
             end
