59ff156709effca31f46255d1028fd80710f4ec4
Chochain Lee <chochain@yahoo.com>
2025-08-02 05:30:54 -0700

    ~/source/spram32_32k.sv rename vo32a,b=>vo0,1

--- source/spram32_32k.sv
+++ source/spram32_32k.sv
@@ -9,7 +9,7 @@ module spram32_32k (                 /// width cascade
         mb32_io.slave b32_if         /// 32-bit bus slave
     );
     logic [3:0]  msk0, msk1;         /// byte select mask
-    logic [31:0] vo32a, vo32b;
+    logic [31:0] vo0, vo1;
     logic cs;
 
     assign cs   = b32_if.ai[14:14];   ///< select high range
@@ -19,10 +19,10 @@ module spram32_32k (                 /// width cascade
     assign msk1 =  {
         b32_if.bmsk[3:3], b32_if.bmsk[3:3], b32_if.bmsk[2:2], b32_if.bmsk[2:2]
     };
-    assign b32_if.vo = cs ? vo32b : vo32a;
+    assign b32_if.vo = cs ? vo1 : vo0;
    
 //    always_ff @(posedge b32_if.clk) begin
-//       b32_if.vo <= cs ? vo32b : vo32a;     /// latch needed? (says yosys)
+//       b32_if.vo <= cs ? vo1 : vo0;     /// latch needed? (says yosys)
 //    end
 
     SP256K bank00 (
@@ -35,7 +35,7 @@ module spram32_32k (                 /// width cascade
         .STDBY(1'b0),                /// STDBY(cs) to save power
         .SLEEP(1'b0),                /// SLEEP(cs) to save power
         .PWROFF_N(1'b1),             /// POWER on
-        .DO(vo32a[15:0])
+        .DO(vo0[15:0])
     );
     SP256K bank01 (
         .AD(b32_if.ai[13:0]),
@@ -47,7 +47,7 @@ module spram32_32k (                 /// width cascade
         .STDBY(1'b0),                /// can use cs to save power
         .SLEEP(1'b0),
         .PWROFF_N(1'b1),
-        .DO(vo32a[31:16])
+        .DO(vo0[31:16])
     );
     SP256K bank10 (
         .AD(b32_if.ai[13:0]),
@@ -59,7 +59,7 @@ module spram32_32k (                 /// width cascade
         .STDBY(1'b0),                /// can use ~cs to save power
         .SLEEP(1'b0),
         .PWROFF_N(1'b1),
-        .DO(vo32b[15:0])
+        .DO(vo1[15:0])
     );
     SP256K bank11 (
         .AD(b32_if.ai[13:0]),
@@ -71,7 +71,7 @@ module spram32_32k (                 /// width cascade
         .STDBY(1'b0),                /// can use ~cs to save power
         .SLEEP(1'b0),
         .PWROFF_N(1'b1),
-        .DO(vo32b[31:16])
+        .DO(vo1[31:16])
     );
 endmodule : spram32_32k
 
