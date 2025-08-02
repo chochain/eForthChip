0fea1b103b58dc96254cd8d4ece3ac42114b5c2f
Chochain Lee <chochain@yahoo.com>
2025-08-02 15:39:07 +0800

    ~/source add mb8_io, mb32_io (yosys do not support parametric)

---
+++ source/mb8_io.sv
@@ -0,0 +1,16 @@
+///
+/// @file
+/// @brief universal memory bus interface - paramatric sizing
+/// @note use modport to regulate the usage
+///
+interface mb8_io #(                     // generic memory block interface
+        parameter   DSZ = 8             // 8, 16, 32-bit bus
+    ) (                                        
+        input logic clk
+    );
+    localparam ASZ = 20 - $clog2(DSZ); // 17, 16, 15 (for 128K SPRAM)
+    logic           we;                // interface ports are bidirectional by default
+    logic [ASZ-1:0] ai;                // specifying either master or slave device
+    logic [DSZ-1:0] vi;
+    logic [DSZ-1:0] vo;
+endinterface: mb8_io
