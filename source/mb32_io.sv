79011826856b8603efef8ef1f6fa89f27ab6bd4b
Chochain Lee <chochain@yahoo.com>
2025-08-02 16:20:11 +0800

    ~/source/mb32_io.sv param=>localparm

--- source/mb32_io.sv
+++ source/mb32_io.sv
@@ -3,17 +3,14 @@
 /// @brief universal memory bus interface - paramatric sizing
 /// @note use modport to regulate the usage
 ///
-interface mb32_io #(                   // generic memory block interface
-        parameter DSZ = 32             // 8, 16, 32-bit bus
-    ) (
+interface mb32_io (                   // generic memory block interface
         input logic clk
     );
+    localparam DSZ = 32;               // 8, 16, 32-bit bus
     localparam ASZ = 20 - $clog2(DSZ); // 17, 16, 15 (for 128K SPRAM)
     logic           we;                // interface ports are bidirectional by default
     logic [ASZ-1:0] ai;                // specifying either master or slave device
     logic [DSZ-1:0] vi;
     logic [DSZ-1:0] vo;
-    /* verilator lint_off UNUSEDSIGNAL */
-    logic [3:0]      bmsk;             // 8-bit does not need this
-    /* verilator lint_on UNUSEDSIGNAL  */
+    logic [3:0]     bmsk;
 endinterface: mb32_io
