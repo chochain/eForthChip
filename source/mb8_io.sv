73093f83964654888d93ab633ea945391c06c347
Chochain Lee <chochain@yahoo.com>
2025-08-02 16:20:36 +0800

    ~/source/mb32_io.sv param=>localparm

--- source/mb8_io.sv
+++ source/mb8_io.sv
@@ -3,11 +3,10 @@
 /// @brief universal memory bus interface - paramatric sizing
 /// @note use modport to regulate the usage
 ///
-interface mb8_io #(                     // generic memory block interface
-        parameter   DSZ = 8             // 8, 16, 32-bit bus
-    ) (                                        
+interface mb8_io (                     // generic memory block interface
         input logic clk
     );
+    localparam DSZ = 8;                // 8-bit bus
     localparam ASZ = 20 - $clog2(DSZ); // 17, 16, 15 (for 128K SPRAM)
     logic           we;                // interface ports are bidirectional by default
     logic [ASZ-1:0] ai;                // specifying either master or slave device
